ad6ad8c940b433ef86456fcc74c9203e9070b493
Tonnix <kwajeanthony@gmail.com>
2021-02-27 15:10:26 +0300

    Updated reporting tables name varchar size

--- code/services/reporting-tables/src/main/resources/reporting_tables.sql
+++ code/services/reporting-tables/src/main/resources/reporting_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE reporting_table (
     id SERIAL UNIQUE PRIMARY KEY NOT NULL,
     reporting_framework_id INTEGER NOT NULL,
     number VARCHAR(50) NOT NULL,
-    name VARCHAR(250) NOT NULL,
+    name VARCHAR NOT NULL,
     description VARCHAR NULL,
     version INTEGER NOT NULL);
 
