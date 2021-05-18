9692ddc96a09f206c6e8a81a2503b0ba3327f338
Tonnix <kwajeanthony@gmail.com>
2021-05-19 01:28:49 +0300

    included the home layout

--- code/client/src/modules/navigation/layouts/index.ts
+++ code/client/src/modules/navigation/layouts/index.ts
@@ -1,16 +1,19 @@
 import { LayoutAuthComponent } from './layout-auth/layout-auth.component';
 import { LayoutDashboardComponent } from './layout-dashboard/layout-dashboard.component';
 import { LayoutErrorComponent } from './layout-error/layout-error.component';
+import { LayoutHomeComponent } from './layout-home/layout-home.component';
 import { LayoutMainComponent } from './layout-main/layout-main.component';
 
 export const layouts = [
     LayoutAuthComponent,
     LayoutDashboardComponent,  
-    LayoutErrorComponent,  
+    LayoutErrorComponent, 
+    LayoutHomeComponent, 
     LayoutMainComponent];
 
 
 export * from './layout-auth/layout-auth.component';
 export * from './layout-dashboard/layout-dashboard.component';
 export * from './layout-error/layout-error.component';
+export * from './layout-home/layout-home.component';
 export * from './layout-main/layout-main.component';
