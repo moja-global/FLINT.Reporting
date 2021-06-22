81b72d26930fb1d8dd5c047e5e250bd029784888
Tonnix <kwajeanthony@gmail.com>
2021-06-22 15:58:30 +0300

    Added ops scripts

---
+++ code/scripts/operations/services/start_data_processor.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+echo 
+echo "************************************************************************"
+echo " Entering Start Data Integration Script"
+echo "************************************************************************"
+echo 
+
+# ------------------------------------------------------------------------
+# INITIALIZE SHELL COLOR VARIABLES
+# ------------------------------------------------------------------------
+
+RED_COLOR='\033[0;31m'
+GREEN_COLOR='\033[0;32m'
+NO_COLOR='\033[0m'
+
+# ------------------------------------------------------------------------
+# INITIALIZE PATH VARIABLES
+# ------------------------------------------------------------------------
+
+
+# root/project/scripts/setup/system/services
+SERVICES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# root/project/scripts/setup/system
+SYSTEM_DIR="$(dirname "$SERVICES_DIR")"
+
+# root/project/scripts/setup
+SETUP_DIR="$(dirname "$SYSTEM_DIR")"
+
+# root/project/scripts
+SCRIPTS_DIR="$(dirname "$SETUP_DIR")"
+
+# root/project
+PROJECT_DIR="$(dirname "$SCRIPTS_DIR")"
+
+
+# ------------------------------------------------------------------------
+# INITIALIZE MICROSERVICES ARRAY - IN DEPENDENCY-SENSITIVE ORDER
+# ------------------------------------------------------------------------
+
+MICROSERVICES=("data-processor")
+
+
+
+# ------------------------------------------------------------------------
+# INSTALL MICROSERVICES
+# ------------------------------------------------------------------------
+
+for MICROSERVICE in "${MICROSERVICES[@]}"; do
+
+	bash $PROJECT_DIR/services/$MICROSERVICE/install.sh
+
+done
+
+
+
+echo 
+echo "************************************************************************"
+echo " Leaving Start Data Integration Script"
+echo "************************************************************************"
+echo 
---
+++ code/scripts/operations/services/stop_data_aggregator.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo 
+echo "************************************************************************"
+echo " Entering Stop Data Integration Script"
+echo "************************************************************************"
+echo 
+
+# ------------------------------------------------------------------------
+# INITIALIZE SHELL COLOR VARIABLES
+# ------------------------------------------------------------------------
+
+RED_COLOR='\033[0;31m'
+GREEN_COLOR='\033[0;32m'
+NO_COLOR='\033[0m'
+
+# ------------------------------------------------------------------------
+# INITIALIZE PATH VARIABLES
+# ------------------------------------------------------------------------
+
+
+# root/project/scripts/setup/system/services
+SERVICES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# root/project/scripts/setup/system
+SYSTEM_DIR="$(dirname "$SERVICES_DIR")"
+
+# root/project/scripts/setup
+SETUP_DIR="$(dirname "$SYSTEM_DIR")"
+
+# root/project/scripts
+SCRIPTS_DIR="$(dirname "$SETUP_DIR")"
+
+# root/project
+PROJECT_DIR="$(dirname "$SCRIPTS_DIR")"
+
+
+# ------------------------------------------------------------------------
+# INITIALIZE MICROSERVICES ARRAY - IN DEPENDENCY-SENSITIVE ORDER
+# ------------------------------------------------------------------------
+
+MICROSERVICES=("data-aggregator")
+
+
+
+# ------------------------------------------------------------------------
+# UNINSTALL MICROSERVICES
+# ------------------------------------------------------------------------
+
+for ((i=${#MICROSERVICES[@]}-1; i>=0; i--)); do
+	bash $PROJECT_DIR/services/${MICROSERVICES[$i]}/uninstall.sh
+done
+
+
+
+echo 
+echo "************************************************************************"
+echo " Leaving Stop Data Integration Script"
+echo "************************************************************************"
+echo 
