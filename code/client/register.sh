fc4e31b3378a00bea6589d61f34097be6335b36b
Simpleshell3 <92247771+Simpleshell3@users.noreply.github.com>
2022-02-13 04:55:25 -0600

    Update register.sh
    Signed-off-by: Simpleshell3 <simpleshell3@gmail.com>
    Signed-off-by: root <root@reporter.miles.co.ke>

--- code/client/register.sh
+++ code/client/register.sh
@@ -1,42 +1,53 @@
 #!/bin/bash
 
+echo 
+echo "========================================================================"
+echo "Entering Artifact Registration Script"
+echo "========================================================================"
 
-echo
-echo "---------------------------------------------------------------------------------"
-echo "Registering component"
-echo "---------------------------------------------------------------------------------"
+# ------------------------------------------------------------------------
+# INITIALIZE SHELL COLOR VARIABLES
+# ------------------------------------------------------------------------
 
+RED_COLOR='\033[0;31m'
+GREEN_COLOR='\033[0;32m'
+NO_COLOR='\033[0m'
 
-
-# -------------------------------------------------------------------------------------
-# INITIALIZE VARIABLES
-# -------------------------------------------------------------------------------------
-
-echo
-echo "Initializing variables"
+# ------------------------------------------------------------------------
+# INITIALIZE PATH VARIABLES
+# ------------------------------------------------------------------------
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
+CODE_DIR="$(dirname "$BASEDIR")"
+ROOT_DIR="$(dirname "$CODE_DIR")"
 
 
+# ------------------------------------------------------------------------
+# SET DEFAULT WORKING DIRECTORY
+# ------------------------------------------------------------------------
 
-# -------------------------------------------------------------------------------------
-# SET WORKING DIRECTORY
-# -------------------------------------------------------------------------------------
-
-echo
-echo "Setting the working directory"
 cd $BASEDIR
 
 
+# ------------------------------------------------------------------------
+# INITIALIZE SERVER / ARTIFACTS VARIABLES
+# ------------------------------------------------------------------------
 
-# -------------------------------------------------------------------------------------
-# BUILD, TAG & PUSH VERSIONED IMAGE
-# -------------------------------------------------------------------------------------
+echo 
+REGISTRY_SERVER=$(jq -r '.domains.registry' $ROOT_DIR/code/configurations/configurations.json)
+if [[ $REGISTRY_SERVER == null ]]
+then
+     echo 
+     echo -e "${RED_COLOR}Registry Server's Configuration is Missing${NO_COLOR}"
+     echo 
+     echo "------------------------------------------------------------------------"
+     echo "Aborting Artifact Registration"
+     echo "------------------------------------------------------------------------"
+     echo 
 
-echo
-echo "Building, tagging & pushing versioned image"
-echo
-docker build -t systems.ags.co.ke:5043/client:0.0.1 $BASEDIR
-docker push systems.ags.co.ke:5043/client:0.0.1
+     exit 1
+else
+     echo -e "${GREEN_COLOR}REGISTRY = ${REGISTRY_SERVER} ${NO_COLOR}"
+fi
 
 
 
@@ -47,12 +58,11 @@ docker push systems.ags.co.ke:5043/client:0.0.1
 #echo
 #echo "Building, tagging & pushing latest image"
 #echo
-#docker build -t ${REGISTRY_SERVER_HOST_NAME}:5043/client:latest $BASEDIR
-#docker push ${REGISTRY_SERVER_HOST_NAME}:5043/client:latest
+docker build -t ${REGISTRY_SERVER}:5043/client:latest $BASEDIR
+docker push ${REGISTRY_SERVER}:5043/client:latest
 
 
-
-echo
-echo "---------------------------------------------------------------------------------"
-echo "Done registering component"
-echo "---------------------------------------------------------------------------------"
+echo 
+echo "========================================================================"
+echo "Leaving Artifact Registration Script"
+echo "========================================================================"
